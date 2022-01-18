6dae914a061c8cf865f1bc65a99fc7e3d4464282
mrcs22 <marcusmoraes2010@hotmail.com>
2022-01-18 10:46:49 -0300

    Preparando revisão

--- src/components/NewButton/index.jsx
+++
@@ -1,8 +0,0 @@
-import { New } from "./style";
-
-function Newbutton({action}){
-    return <New onClick={action}> + </New> 
-}
-
-
-export default Newbutton;
--- src/contexts/CredentialContext.jsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-const CredentialContext = createContext();
-
-
-export default CredentialContext;
